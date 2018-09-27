23f01cf068c3ed0581aedbf18c5091c7ed68d9b7
pjwilcoxen <wilcoxen@syr.edu>
2018-09-27 15:18:19 -0400

    Increment version number.

--- src/build.h
+++ src/build.h
@@ -1,7 +1,7 @@
 #ifndef BUILD_H
 #define BUILD_H
 
-static char *verstr = "build 711 (2018/09/27)" ;
-static char *gitver = "v2.6.1" ;
+static char *verstr = "build 712 (2018/09/27)" ;
+static char *gitver = "v2.7.0" ;
 
 #endif /* BUILD_H */
