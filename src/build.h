74fd72cabe0e1769f374fe74d51e21defef5bc71
pjwilcoxen <wilcoxen@syr.edu>
2019-09-29 21:56:59 -0400

    Update release number.

--- src/build.h
+++ src/build.h
@@ -1,7 +1,7 @@
 #ifndef BUILD_H
 #define BUILD_H
 
-static char *verstr = "build 749 (2019/09/19)" ;
-static char *gitver = "v2.7.3" ;
+static char *verstr = "build 750 (2019/09/29)" ;
+static char *gitver = "v2.7.4" ;
 
 #endif /* BUILD_H */
