7593fd750d4a76ff3ba1a700d6fea1af50c66344
galexy <geoff@galexy.net>
2022-11-22 16:25:58 +1100

    First start on python.c lang file

--- src/lang/setup.c
+++ src/lang/setup.c
@@ -8,6 +8,8 @@ void Oxnewton_setup(void);
 void Troll_setup(void);
 void Tablo_setup(void);
 void Html_setup(void);
+// GCS 2022-11-22 addition for Python version of GCubed
+void Python_setup(void);
 
 extern char *Debug_version;
 extern char *Msgproc_version;
@@ -17,15 +19,19 @@ extern char *Oxnewton_version;
 extern char *Troll_version;
 extern char *Tablo_version;
 extern char *Html_version;
+// GCS 2022-11-22 addition for Python version of GCubed
+extern char *Python_version;
 
 void setup_languages()
 {
-   addlang( "debug"    , Debug_setup    , Debug_version    );
-   addlang( "msgproc"  , Msgproc_setup  , Msgproc_version  );
-   addlang( "oxgs"     , OxGS_setup     , OxGS_version     );
-   addlang( "oxgst"    , OxGST_setup    , OxGST_version    );
-   addlang( "oxnewton" , Oxnewton_setup , Oxnewton_version );
-   addlang( "troll"    , Troll_setup    , Troll_version    );
-   addlang( "tablo"    , Tablo_setup    , Tablo_version    );
-   addlang( "html"     , Html_setup     , Html_version     );
+   addlang("debug", Debug_setup, Debug_version);
+   addlang("msgproc", Msgproc_setup, Msgproc_version);
+   addlang("oxgs", OxGS_setup, OxGS_version);
+   addlang("oxgst", OxGST_setup, OxGST_version);
+   addlang("oxnewton", Oxnewton_setup, Oxnewton_version);
+   addlang("troll", Troll_setup, Troll_version);
+   addlang("tablo", Tablo_setup, Tablo_version);
+   addlang("html", Html_setup, Html_version);
+   // GCS 2022-11-22 addition for Python version of GCubed
+   addlang("python", Python_setup, Python_version);
 }
